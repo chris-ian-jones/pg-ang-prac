5e8cb59fad436167d0918c80cc47fb343c2b3249
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-25 13:46:57 +0100

    WIP: setup NavBar, and model for Course

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { GalleryComponent } from './gallery/gallery.component';
+import { CourseCardComponent } from './course-card/course-card.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    GalleryComponent,
+    CourseCardComponent
   ],
   imports: [
     BrowserModule,
