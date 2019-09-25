9e19d7989864ef3f025ec17ddda39fcdff14c107
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-09-25 14:14:23 +0100

    WIP: displaying course data received from api

--- src/app/course-service.service.ts
+++ src/app/course-service.service.ts
@@ -2,12 +2,23 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http'
 
+import { Course } from './models/Course'
 
+const httpOptions = {
+  headers: new HttpHeaders({
+    'Content-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
-export class CourseServiceService {
+export class CourseService {
+  courseUrl:string = 'http://localhost:6789/api/courses'
+  
+  constructor(private http:HttpClient) { }
 
-  constructor() { }
+  getCourses():Observable<Course[]> {
+    return this.http.get<Course[]>(this.courseUrl)
+  }
 }
--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CourseService } from './../course-service.service'
+import { Course } from './../models/Course'
+
 @Component({
   selector: 'app-gallery',
   templateUrl: './gallery.component.html',
   styleUrls: ['./gallery.component.css']
 })
 export class GalleryComponent implements OnInit {
+  courses:Course[]
 
-  constructor() { }
+  constructor(private courseService:CourseService) { }
 
   ngOnInit() {
+    this.courseService.getCourses().subscribe(courses => {
+      this.courses = courses
+    })
   }
 
 }
